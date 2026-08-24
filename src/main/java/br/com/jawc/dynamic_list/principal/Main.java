0a6281f940bfb2998d808bb2bd7ed06806c2390a
jawc-05 <joaoalfredowc@gmail.com>
2026-08-24 00:03:31 -0300

    test: tested the new methods, everything worked fine

--- src/main/java/br/com/jawc/dynamic_list/principal/Main.java
+++ src/main/java/br/com/jawc/dynamic_list/principal/Main.java
@@ -3,6 +3,7 @@
  */
 package main.java.br.com.jawc.dynamic_list.principal;
 
+import main.java.br.com.jawc.dynamic_list.model.Informacao;
 import main.java.br.com.jawc.dynamic_list.structure.ListaDinamica;
 
 public class Main {
@@ -10,7 +11,28 @@ public class Main {
     public static void main(String[] args) {
         ListaDinamica lista = new ListaDinamica();
 
-        System.out.println("Lista criada! Vazia? " + lista.isEmpty());
+        System.out.println("--- TESTE DE INSERÇÃO ORDENADA ---");
+
+        // 1. Inserindo o primeiro (Cenário: Lista Vazia)
+        lista.inserirOrdenado(new Informacao("Carlos"));
+
+        // 2. Inserindo antes do primeiro (Cenário: Início)
+        lista.inserirOrdenado(new Informacao("Ana"));
+
+        // 3. Inserindo depois de todos (Cenário: Fim)
+        lista.inserirOrdenado(new Informacao("Zebra"));
+
+        // 4. Inserindo no meio (Cenário: Meio)
+        lista.inserirOrdenado(new Informacao("Beto"));
+
+        // 5. Tentando inserir duplicado (Cenário: Duplicata)
+        lista.inserirOrdenado(new Informacao("Ana"));
+
+        System.out.println("\nExibindo na ordem normal (usando elop):");
+        lista.exibir(); // Esperado: [Ana] [Beto] [Carlos] [Zebra]
+
+        System.out.println("\nExibindo na ordem inversa (usando eloa):");
+        lista.exibirInverso(); // Esperado: [Zebra] [Carlos] [Beto] [Ana]
 
     }
 }

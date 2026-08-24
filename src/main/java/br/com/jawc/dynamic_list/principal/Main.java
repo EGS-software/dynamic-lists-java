7a96b508c9bfad18d5dd09f60c7cd132ce8bf8a1
jawc-05 <joaoalfredowc@gmail.com>
2026-08-23 23:04:42 -0300

    feat: added principal pkg and Main class

---
+++ src/main/java/br/com/jawc/dynamic_list/principal/Main.java
@@ -0,0 +1,16 @@
+/**
+ * @author jawc-05
+ */
+package main.java.br.com.jawc.dynamic_list.principal;
+
+import main.java.br.com.jawc.dynamic_list.structure.ListaDinamica;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ListaDinamica lista = new ListaDinamica();
+
+        System.out.println("Lista criada! Vazia? " + lista.isEmpty());
+
+    }
+}
